887ef8a90b9f24c074e6dfb7714df17cfb0fc52e
Garrett Dickinson <dickinsongarrett@gmail.com>
2020-02-08 19:54:49 -0600

    Rewrite Parser

--- Parser.java
+++ Parser.java
@@ -2,28 +2,47 @@ import java.util.ArrayList;
 
 public class Parser {
     
-    //ArrayList<String> ;
+    ArrayList<String> keywords = new ArrayList<String>() {
+        "add",
+        "sub",
+        "mult",
+        "div",
+        "mod",
+        "fact",
+        "pow",
+        "pi",
+        "log",
+        "rand"
+    };
+
+    ArrayList<String> std_functions = new ArrayList<String>() {
+        "print"
+    };
     
     public Parser(ArrayList<String> script) {
         for (String line : script) {
-            if (line.contains("print")) {
-                if (!line.endsWith(";")) {
-                    System.out.println("Error: Missing ; at end of line [" + script.indexOf(line) + ", " + (line.length() - 1) + "]\n");
-                    System.out.println(line);
-                    String errorPos = "";
-                    for (int x = 0; x < line.length(); x++) {
-                        errorPos += " ";
-                    }
-                    System.out.println(errorPos + "^");
-                    return;
-                } else {
-                    String parameter = line.trim().substring(5);
-                    System.out.println(parameter);
-                }
-                
-            } else if (line.contains("")) {
-
+            // Seperate all of our words in a line
+            String[] words = line.split(" ");
+            // Look for keyword at first word, split away the parentheses
+            //System.out.println(words);
+            System.out.println(words[0]);
+            if (words[0].equals("print")) {
+                //We have a printfunction
+                System.out.println("This is a print statement");
+            } else {
+                System.out.println("This is NOT a print statement");
             }
+            
+            // if (line.contains("printme")) {
+                
+            // } else {
+            //     String parameter = line.trim().substring(5);
+            //     System.out.println(parameter);
+            // }
+                
+            // } else if (line.contains("")) {
+
+            // }
         }
     }
 }
