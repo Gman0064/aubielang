09ebecfc9c5a01ef681015e948f5d9b1fbd998e7
Sakshamgoel <53540005+Sakshamgoel@users.noreply.github.com>
2020-02-09 01:28:09 -0600

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Variable implements Comparable<Variable>
+{
+	String name;
+	String type;
+	String value;
+	public Variable(String nameIn, String typeIn, String valueIn)
+	{
+		name = nameIn;
+		type = typeIn;
+		valueIn = value;
+	}
+	public boolean equals(Variable varIn)
+	{
+		if(!this.type.equals(varIn.type))
+		{
+			return false;
+		}
+		return this.value.equals(varIn.value);
+	}
+	public int compareTo(Variable varIn)
+	{
+		if(!type.equals(varIn.type))
+		{
+			throw new Error();
+		}
+		if (this.type.equals("num")) {
+			return Double.compare(Double.parseDouble(this.value), Double.parseDouble(varIn.value));
+		} else {
+			return this.value.compareTo(varIn.value);
+		}
+	}
+}
