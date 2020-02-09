0a29928d7c54c31f3c68ef4b7b14f829c463bce1
jrc0114 <54918464+jrc0114@users.noreply.github.com>
2020-02-09 02:48:29 -0600

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 public class Parser {
     public Scanner commands;
-    public static HashMap<String, Object[]> variables = new HashMap<String, Object[]>();
+    public static HashMap<String,Variable> vars = new HashMap<String,Variable>();
 
-    LangMath math = new LangMath();
-    LangUtil util = new LangUtil();
 
-    // Variable Example: {'var1': ['num',12.0], 'var2': ['string','Hello']}
     public Parser(Scanner input) {
         commands = input;
     }
 
     public void main() {
         while (commands.hasNext()) {
-            System.out.println(parse(commands.nextLine().trim()));
+            parse(commands.nextLine().trim());
         }
     }
 
@@ -22,88 +19,273 @@ public class Parser {
         String[] stateSplit = statement.split(" ");
         String command = stateSplit[0];
         String rest = "";
-        if (statement.length() >= command.length() + 1) {
-            rest = statement.substring(command.length() + 1);
-        } else {
-            throw new Error();
-        }
-        if (command.equals("def")) {
-            if (def(rest)) {
-                return "Defined";
-            } else {
-                return "Incorrect statement";
+        
+        if (stateSplit.length == 1) {
+            if ((command.charAt(0) == command.charAt(command.length() - 1)) && (command.charAt(0) == '"')) {
+                return command.substring(1,command.length()-1);
             }
-        } else if (command.equals("sum")) {
-            return math.sum(rest);
-        } else if (command.equals("sub")) {
-            return math.subtract(rest);
-        } else if (command.equals("mult")) {
-            return math.mult(rest);
-        } else if (command.equals("pow")) {
-            return math.power(rest);
-        } else if (command.equals("mod")) {
-            return math.mod(rest);
-        } else if (command.equals("log")) {
-            return math.log(rest);
-        } else if (command.equals("div")) {
-            return math.div(rest);
-        } else if (command.equals("fact")) {
-            return math.fact(rest);
-        } else if (command.startsWith("#")) {
-            return "Commented Line";
-        } else if (command.equals("")) {
-            return "";
-        }
+            else if (isDefined(command)) {
+                if (vars.get(command).type.equals("num")) {
+                    return parse(vars.get(command).value);
+                } else {
+                    return vars.get(command).value;
+                }
+            }
+            else {
+                return Double.parseDouble(command);
+            }
+        } else {
+            rest = statement.substring(command.length() + 1);
+            if (command.equals("print")) {
+                Object result = parse(rest);
+                System.out.println(result.toString());
+                return result;
+            } else if (command.equals("def")) {
+                if (def(rest)) {
+                   return "Defined";
+                }
 
-        throw new Error();
+            } else if (command.equals("sum")) {
+                return sum(rest);
+
+            } else if (command.equals("sub")) {
+                return subtract(rest);
+
+            } else if (command.equals("mult")) {
+                return mult(rest);
+
+            } else if (command.equals("pow")) {
+                return power(rest);
+
+            } else if (command.equals("mod")) {
+                return mod(rest);
+
+            } else if (command.equals("log")) {
+                return log(rest);
+
+            } else if (command.equals("div")) {
+                return div(rest);
+
+            } else if (command.equals("fact")) {
+                return fact(rest);
+
+            } else if (command.startsWith("#")) {
+                return "Commented Line";
+
+            } else if (command.equals("")) {
+                return "";
+            }
+        }
+        throw new Error("Hi");
+    }
+
+    public boolean isDefined(String name) {
+        if (vars.containsKey(name)) return true;
+        return false;
     }
 
     public boolean def(String args) {
+        System.out.println(args);
+        // name type value
         String[] arguments = args.split(" ");
-        if (arguments.length < 3) {
-            return false;
-        }
-
+        if (arguments.length < 3) throw new Error();
+        
         String name = arguments[0];
         String type = arguments[1];
-        if (!(type.equals("num") || type.equals("string"))) {
-            System.out.println("   Parsing Error: Type \"" + type + "\" not recognized");
-            throw new Error();
+
+        if (!(type.equals("num") || type.equals("string"))) throw new Error();
+
+        String args3 = "";
+        for (int i = 2; i < arguments.length; i++) {
+            args3 += arguments[i] + " ";
         }
-        Object value;
-        if (arguments.length == 3) {
-            value = arguments[2];
-            if (variables.containsKey(value)) {
-                if (!type.equals(variables.get(value)[0])) {
-                    System.out.println("   Parsing Error: Types do not match");
-                    throw new Error();
-                } else {
-                    Object[] arr = { type, variables.get(value)[1] };
-                    variables.put(name, arr);
-                }
-            } else {
-                if (type.equals("num")) {
-                    Double numVal = Double.parseDouble(value.toString());
-                    variables.put(name, new Object[] { type, numVal });
-                    return true;
-                } else {
-                    variables.put(name, new Object[] { type, value });
-                    return true;
-                }
-            }
+        if (type.equals("num")) {
+            Double value = (Double) parse(args3.trim());
+            vars.put(name,new Variable(type,value.toString()));
+            return true;
         } else {
-            String nArgs = "";
-            String[] newArgs = new String[arguments.length - 2];
-            for (int i = 2; i < arguments.length; i++) {
-                newArgs[i - 2] = arguments[i];
-            }
-            for (String arg : newArgs) {
-                nArgs += arg + " ";
-            }
-            value = parse(nArgs.trim());
-            variables.put(name, new Object[] { type, value });
+            String value = (String) parse(args3.trim());
+            vars.put(name,new Variable(type,value.toString()));
             return true;
         }
-        return false;
+    }
+    public double mult(String statement) {
+        double product = 1;
+        String[] words = statement.split(" ");
+        ArrayList<Double> nums = new ArrayList<Double>();
+        //Iterate through parameters of our operator
+        for (int x = 0; x < words.length; x++) {
+            try {
+                Double num = (Double) parse(words[x]);
+                nums.add(num);
+            } catch(Exception e) {
+                throw new Error("Invalid element type");
+            }
+        }
+        //Multiply our numbers
+        for (int x = 0; x < nums.size(); x++) {
+            product *= nums.get(x);
+        }
+        return product;
+    }
+
+    public double sum(String statement) {
+        double sum = 0;
+        String[] words = statement.split(" ");
+        ArrayList<Double> nums = new ArrayList<Double>();
+        // Iterate through our string for numbers to add
+        for (int x = 0; x < words.length; x++) {
+            try {
+                Double num = (Double) parse(words[x]);
+                nums.add(num);
+            } catch(Exception e) {
+                throw new Error("Invalid element type");
+            }
+        }
+        for (int x = 0; x < nums.size(); x++) {
+            sum += nums.get(x);
+        }
+        return sum;
+    }
+
+    public double power(String statement) {
+        double pow = 1;
+        String[] words = statement.split(" ");
+        ArrayList<Double> nums = new ArrayList<Double>();
+        // Iterate through our string for numbers to add
+        if (words.length <= 2) {
+            for (int x = 0; x < words.length; x++) {
+                try {
+                    Double num = (Double) parse(words[x]);
+                    nums.add(num);
+                } catch(Exception e) {
+                    throw new Error("Invalid element type");
+                }
+            }
+            pow = Math.pow(nums.get(0), nums.get(1));
+            return pow;
+        } else {
+            throw new Error("Invalid arguments");
+        }
+        
+    }
+
+    public double subtract(String statement) {
+        double result = 0;
+        String[] words = statement.split(" ");
+        ArrayList<Double> nums = new ArrayList<Double>();
+        // Iterate through our string for numbers to add
+        for (int x = 0; x < words.length; x++) {
+            try {
+                Double num = (Double) parse(words[x]);
+                nums.add(num);
+            } catch(Exception e) {
+                throw new Error("Invalid element type");
+            }
+        }
+        for (int x = 0; x < nums.size(); x++) {
+            if (x == 0) {
+                result = nums.get(0);
+            } else {
+                result -= nums.get(x);
+            }
+        }
+        return result;
+    }
+
+    public double mod(String statement) {
+        double modulus = -1.0;
+        String[] words = statement.split(" ");
+        if (words.length > 2) {
+            throw new Error("Invalid arguments");
+        } else {
+            ArrayList<Double> nums = new ArrayList<Double>();
+            // Iterate through our string for numbers to add
+            for (int x = 0; x < 2; x++) {
+                try {
+                    Double num = (Double) parse(words[x]);
+                    nums.add(num);
+                } catch(Exception e) {
+                    throw new Error("Invalid element type");
+                }
+            }
+            modulus = nums.get(0);
+            Double numB = nums.get(1);
+            while (modulus >= numB) {
+                modulus = modulus - numB;
+            }
+        }
+        return modulus;
+    }
+
+    public double div(String statement) {
+        double div = 1;
+        String[] words = statement.split(" ");
+        ArrayList<Double> nums = new ArrayList<Double>();
+        // Iterate through our string for numbers to add
+        for (int x = 0; x < words.length; x++) {
+            try {
+                Double num = (Double) parse(words[x]);
+                nums.add(num);
+            } catch(Exception e) {
+                throw new Error("Invalid element type");
+            }
+        }
+
+        for (int x = 0; x < nums.size(); x++) {
+            if (x == 0) {
+                div = nums.get(0);
+            } else {
+                div /= nums.get(x);
+            }
+        }
+        return div;
+    }
+
+    public double log(String statement) {
+        double logVal = 0.0;
+        String[] words = statement.split(" ");
+        if (words.length > 2) {
+            throw new Error("Invalid arguments");
+        } else {
+            ArrayList<Double> nums = new ArrayList<Double>();
+            // Iterate through our string for numbers to add
+            for (int x = 0; x < 2; x++) {
+                try {
+                    Double num = (Double) parse(words[x]);
+                    nums.add(num);
+                } catch(Exception e) {
+                    throw new Error("Invalid element type");
+                }
+            }
+            logVal = Math.log(nums.get(1)) / Math.log(nums.get(0));
+        }
+        return logVal;
+    }
+
+    public double fact(String statement) {
+        double result = 0;
+        String[] words = statement.split(" ");
+        ArrayList<Double> nums = new ArrayList<Double>();
+
+        //Iterate through our string for numbers to add
+        for (int x = 0; x < words.length; x++) {
+            try {
+                Double num = (Double) parse(words[x]);
+                nums.add(num);
+            } catch(Exception e) {
+                throw new Error("Invalid element type");
+            }
+        }
+        result = nums.get(0);
+        if(result == 0) {
+            return 0;
+        } 
+        else {
+            result = 1;
+            for (int i = 1; i <= nums.get(0); i++)
+            result *= i;
+        }
+        return result;     
     }
 }
